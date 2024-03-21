f19733ff5703ed7522c743c6faa74bff89ec1895
Atharv <atharvgoel03@gmail.com>
2024-03-21 21:38:14 +0530

    implemented argparse for arg handling, plus encapsulated node in a func

--- README.md
+++ README.md
@@ -1,19 +1,22 @@
 # DBZ
+
 Hey, I head your database is strong. Let me fight it!
 
 ## Developer Notes
+
 - `node_implementation.py` contains the canonical Node class.
 It also inherits the correct gRPC class so write the handling
 of RPC requests in the class itself.
-- Usage is updated to `python node.py <id> <ip:port>`
-- `raft_init.py` can be broken if so wish, but it makes the node 
+- Usage is updated to `python3 node.py --id <node id> --address <ip:port>`
+  - You can invoke `python3 node.py --help` to see launch options.
+- `raft_init.py` can be broken if so wish, but it makes the node
 files shorter, so let it be.
 - I hope I have not made a mistake.
 - As of now the implementation lacks the skill to handle
 communication channels that become inaccessible (to simulate
 dead nodes.)
 - `raft_init.py` also contains the addresses of all the other
-nodes in the system. That's the thing to update when 
+nodes in the system. That's the thing to update when
 moving to GCP.
 
 Bruh. Writing readmes is hard if just before, you got rekt in gym.
--- node.py
+++ node.py
@@ -1,35 +1,45 @@
-from sys import argv
 from threading import Thread
 import time
 from grpc._channel import _InactiveRpcError
+import argparse
 
 from node_implementation import Node
 from proto import raft_pb2
 from raft_init import confirm, serve, get_node_stubs_other_than
 
-node = Node(int(argv[1]))
-ip, port = argv[2].split(':')
-thread = Thread(target=serve, args=(port, node))
-thread.start()
-time.sleep(1)
-confirm()
 
-node.other_nodes = get_node_stubs_other_than(argv[2])
-for stub in node.other_nodes:
-    args = {
-        "term": 1,
-        "candidate_id": 2,
-        "last_log_index": 3,
-        "last_log_term": 4
-    }
-    try:
-        response = stub.RequestVote(raft_pb2.VoteRequest(**args))
-        print(response.vote_granted)
-    except _InactiveRpcError:
-        print('Node is down!!')
-    while True:
-        pass
+def parse_args():
+    parser = argparse.ArgumentParser(description='Start a Raft node')
+    parser.add_argument('--id', type=int, help='ID of the node')
+    parser.add_argument('--address', type=str, help='Address of the node in the format ip:port')
+    return parser.parse_args()
 
 
+def main(args):
+    node = Node(args.id)
+    port = args.address.split(':')[-1]
+    thread = Thread(target=serve, args=(port, node))
+    thread.start()
+    time.sleep(1)
+    confirm()
+
+    node.other_nodes = get_node_stubs_other_than(args.address)
+    for stub in node.other_nodes:
+        args = {
+            "term": 1,
+            "candidate_id": 2,
+            "last_log_index": 3,
+            "last_log_term": 4
+        }
+        try:
+            response = stub.RequestVote(raft_pb2.VoteRequest(**args))
+            print(response.vote_granted)
+        except _InactiveRpcError:
+            print('Node is down!!')
+        while True:
+            pass
 
 
+if __name__ == '__main__':
+    args = parse_args()
+    main(args)
